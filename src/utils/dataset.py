b5ed2dfb63750e90832136ab4a37170e570890d7
heyongxian <heyongxian@kuaishou.com>
2025-06-06 22:21:33 +0800

    1

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -284,6 +284,20 @@ def get_enhanced_train_transform(img_size=512):
         ToTensorV2(),
     ])
 
+def get_train_transform(img_size=512):
+    """获取训练时的数据增强变换"""
+    return A.Compose([
+        A.Resize(height=img_size, width=img_size),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.2),
+        A.RandomRotate90(p=0.3),
+        A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=15, p=0.3),
+        A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.3),
+        A.HueSaturationValue(hue_shift_limit=10, sat_shift_limit=20, val_shift_limit=10, p=0.3),
+        A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ToTensorV2(),
+    ])
+
 def get_val_transform(img_size=512):
     """获取验证时的数据变换"""
     return A.Compose([
